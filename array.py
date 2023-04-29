fe393ef9649c96e24182370aa983a9ed5ada6f99
Wruthless <wsgnzls@comcast.net>
2023-04-29 15:43:38 -0400

    Initial commit.

---
+++ array.py
@@ -0,0 +1,24 @@
+def maxarray(xs):
+    m = xs[0]
+    for x in xs:
+        if m < x:
+            m = x
+    return m
+
+
+data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+t = maxarray(data)
+print("[+] Max: " + str(t))
+
+def minarray(xs):
+    m = xs[0]
+    for x in xs:
+        if m > x:
+            m = x
+    return m
+
+u = minarray(data)
+print("[+] Min: " + str(u))
+
+
+
---
+++ ifelse.py
@@ -0,0 +1,8 @@
+x = 60
+
+if x > 70:
+    print("gt seventy.")
+elif x >= 60:
+    print("gte sixty.")
+else:
+    print("other.")
